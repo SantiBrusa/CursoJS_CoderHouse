5fa76d90f248102950744fd41e1e5945c3ed2f0c
Santino <santinovissanibrusadin@gmail.com>
2025-06-28 17:39:25 -0300

    Primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+// console.log('funciona')
+
+// prompy --> simular un input
+
+// consola --> simular que se devuelve em html
+
+// prompt("Ingresa un numero");
+// console.log("Hola");
+// alert("Tu numero es");
+
+// Para crear variables: let y const
+
+// iniciador, nombre, =, valor
+
+// let. Puedo cambiar cuando quiera el valor
+// const. No puedo cambiar el valor
+
+let nombre = "Juan";
+
+const apellido = "perez";
+
+console.log(nombre);
+
+console.log(apellido);
+
+nombre = "Cruz";
+
+console.log(nombre);
+
+// // Prompt
+
+// let nomPrompt = prompt("Ingresa un nombre");
+
+// // Operadores // Concatenar String
+
+// // let saludo = "hola " + nomPrompt + " como estas?";
+
+// // console.log(saludo);
+
+// // template literals
+// // backsticks ``
+
+// let saludoPro = `hola ${nomPrompt} como estas?`;
+
+// console.log(saludoPro);
+
+// Operadores matematicos
+
+let n1 = 9;
+let n2 = 2;
+
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+
+console.log(n1 % n2); // devuelve el resto
+
+// Booleanos
+// Operadores de comparacion
+
+let x = 12;
+let z = 16;
+
+// < > == !=
+
+console.log(x != z);
+
+console.log("---------");
+
+let a1 = 12;
+
+let a2 = "12";
+
+// Comparacion simple
+console.log(a1 == a2);
+
+console.log(a1 != a2);
+
+// Comparacion estricta
+console.log(a1 === a2);
+
+console.log(a1 !== a2);
